4af85e4ef56c7306c4c38622eafddd16f88a0880
Vinícius Henriques <viniciuspinholi6@gmail.com>
2023-12-19 14:44:21 -0300

    Roteamento com Slugify, rotamento por atributos, Linkgenerator para o BotaoTagHelper

--- src/AppSemTemplate/Program.cs
+++ src/AppSemTemplate/Program.cs
@@ -1,10 +1,14 @@
 using AppSemTemplate.Data;
+using AppSemTemplate.Extensions;
 using Microsoft.EntityFrameworkCore;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+//builder.Services.AddRouting(options =>
+//    options.ConstraintMap["slugify"] = typeof(RouteSlugifyParameterTransformer));
 
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
@@ -16,6 +20,10 @@ var app = builder.Build();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller:slugify=Home}/{action:slugify=Index}/{id?}");
 
 app.MapControllerRoute(
     name: "default",
