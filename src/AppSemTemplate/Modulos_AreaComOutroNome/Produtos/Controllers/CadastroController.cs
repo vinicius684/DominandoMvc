2cb75a7706d48d9ccdb1862d86fdebc7e4c13bb6
Vinícius Henriques <viniciuspinholi6@gmail.com>
2023-12-19 15:25:55 -0300

    Criadas Áreas Produtos e Vendas

---
+++ src/AppSemTemplate/Modulos_AreaComOutroNome/Produtos/Controllers/CadastroController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppSemTemplate.Modulos_AreaComOutroNome.Produtos.Controllers
+{
+    [Area("Produtos")]
+    public class CadastroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Detalhes(int id)
+        {
+            return View("Index");
+        }
+    }
+}
---
+++ src/AppSemTemplate/Modulos_AreaComOutroNome/Vendas/Controllers/GestaoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppSemTemplate.Modulos_AreaComOutroNome.Vendas.Controllers
+{
+    [Area("Vendas")]
+    [Route("gestao-vendas")]
+    public class GestaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("detalhe-pedido/{id:int}")]
+        public IActionResult Detalhes(int id)
+        {
+            return View("Index");
+        }
+    }
+}
