118a4936457ab6c95a612f732fb551a302936517
Vinícius Henriques <viniciuspinholi6@gmail.com>
2023-12-19 14:27:18 -0300

    Cridos crud, botão TagHelper customizado e View Component contador de elementos da lista

---
+++ src/AppSemTemplate/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using AppSemTemplate.Models;
+
+namespace AppSemTemplate.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<AppSemTemplate.Models.Produto> Produto { get; set; }
+    }
+}
