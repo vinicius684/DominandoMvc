c1e7d105a1465c704dbe2943caea392915cbe863
Vinícius Henriques <viniciuspinholi6@gmail.com>
2023-12-19 15:25:55 -0300

    Criadas Áreas Produtos e Vendas

--- src/AppSemTemplate/Program.cs
+++ src/AppSemTemplate/Program.cs
@@ -1,11 +1,21 @@
-using AppSemTemplate.Data;
+﻿using AppSemTemplate.Data;
 using AppSemTemplate.Extensions;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+// Adicionando suporte a mudan�a de conven��o da rota das areas.
+builder.Services.Configure<RazorViewEngineOptions>(options =>
+{
+    options.AreaViewLocationFormats.Clear();
+    options.AreaViewLocationFormats.Add("/Modulos_AreaComOutroNome/{2}/Views/{1}/{0}.cshtml");
+    options.AreaViewLocationFormats.Add("/Modulos_AreaComOutroNome/{2}/Views/Shared/{0}.cshtml");
+    options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
+});
 
 //builder.Services.AddRouting(options =>
 //    options.ConstraintMap["slugify"] = typeof(RouteSlugifyParameterTransformer));
@@ -24,6 +34,14 @@ app.UseRouting();
 //app.MapControllerRoute(
 //    name: "default",
 //    pattern: "{controller:slugify=Home}/{action:slugify=Index}/{id?}");
+
+//app.MapControllerRoute(//Add Rota Default para Areas - (n�o necess�rio se usar especializada)
+//            name: "areas",
+//            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");//-Adicionando Rota da Arena na Program
+
+//Rota de �reas especializada
+app.MapAreaControllerRoute("AreaProdutos", "Produtos", "Produtos/{controller=Cadastro}/{action=Index}/{id?}");
+app.MapAreaControllerRoute("AreaVendas", "Vendas", "Vendas/{controller=Gestao}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
     name: "default",
