4bf2df3ca4ad4223ae978ae423337ac552f1dc08
Vinícius Henriques <viniciuspinholi6@gmail.com>
2023-12-19 13:02:26 -0300

    Adicionadas pastas MVC, configurações iniciais Program, Rota padrão

--- src/AppSemTemplate/Program.cs
+++ src/AppSemTemplate/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
