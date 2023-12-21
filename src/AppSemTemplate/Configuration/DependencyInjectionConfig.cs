5e429fa8b5211a1905219c6d5e2d9ed6b31b5e66
Vinícius Henriques <viniciuspinholi6@gmail.com>
2023-12-21 15:22:39 -0300

    Extension Methods para separar a classe program em responsabildiades, config ambiente de execução, Outros arquivos de config

---
+++ src/AppSemTemplate/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,24 @@
+﻿using AppSemTemplate.Data;
+using AppSemTemplate.Services;
+using Microsoft.AspNetCore.Identity;
+
+namespace AppSemTemplate.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static WebApplicationBuilder AddDependencyInjectionConfiguration(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();// inje��o de dependencia do TagHelper
+
+            //exemplos ciclos de vida diferentes
+            builder.Services.AddTransient<IOperacaoTransient, Operacao>();
+            builder.Services.AddScoped<IOperacaoScoped, Operacao>();
+            builder.Services.AddSingleton<IOperacaoSingleton, Operacao>();
+            builder.Services.AddSingleton<IOperacaoSingletonInstance>(new Operacao(Guid.Empty));
+
+            builder.Services.AddTransient<OperacaoService>();
+
+            return builder;
+        }
+    }
+}
--- src/AppSemTemplate/Controllers/HomeController.cs
+++ src/AppSemTemplate/Controllers/HomeController.cs
@@ -1,11 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AppSemTemplate.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace AppSemTemplate.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration Configuration;//injeção
+        private readonly ApiConfiguration ApiConfig;
+
+        public HomeController(IConfiguration configuration,
+            IOptions<ApiConfiguration> apiConfig) 
+        {
+            Configuration = configuration;
+            ApiConfig = apiConfig.Value;
+        }
+        //
+
         public IActionResult Index()
         {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");//Obtendo ambiente?
+
+            //1 popular dadoss da appsettings.Dev na APIConfiguration
+            var apiConfig = new ApiConfiguration();
+            Configuration.GetSection(ApiConfiguration.ConfigName).Bind(apiConfig);
+
+            var secret = apiConfig.UserSecret;
+            //
+            //2var user = Configuration[$"{ApiConfiguration.ConfigName}:UserKey"]; assim faria na mão sem usar classe(??) ourta forma de popular..
+
+            //3Mais indicada
+
+            //4?
+            var domain = ApiConfig.Domain;
+
             return View();
         }
     }
