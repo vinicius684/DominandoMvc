ae1f1df7c9f8b7a531fdf7be97c72b4d1a51387a
Vinícius Henriques <viniciuspinholi6@gmail.com>
2024-01-02 15:34:33 -0300

    Estendendo comportamentos do Razor(apenas pessoas autorizadas tem o botão de exclusão e criação habilitados)

---
+++ src/AppSemTemplate/Extensions/ApagaElementoTagHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AppSemTemplate.Extensions
+{
+    [HtmlTargetElement("*", Attributes = "supress-by-claim-name")]
+    [HtmlTargetElement("*", Attributes = "supress-by-claim-value")]
+    public class ApagaElementoTagHelper : TagHelper
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public ApagaElementoTagHelper(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        [HtmlAttributeName("supress-by-claim-name")]
+        public string? IdentityClaimName { get; set; }
+
+        [HtmlAttributeName("supress-by-claim-value")]
+        public string? IdentityClaimValue { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+
+            var temAcesso = CustomAuthorization.ValidarClaimsUsuario(_contextAccessor.HttpContext, IdentityClaimName, IdentityClaimValue);
+
+            if (temAcesso) return;
+
+            output.SuppressOutput();
+        }
+    }
+}
---
+++ src/AppSemTemplate/Extensions/DesabilitaLinkByClaimTagHelper.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AppSemTemplate.Extensions
+{
+    [HtmlTargetElement("*", Attributes = "disable-by-claim-name")]
+    [HtmlTargetElement("*", Attributes = "disable-by-claim-value")]
+    public class DesabilitaLinkByClaimTagHelper : TagHelper
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public DesabilitaLinkByClaimTagHelper(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        [HtmlAttributeName("disable-by-claim-name")]
+        public string? IdentityClaimName { get; set; }
+
+        [HtmlAttributeName("disable-by-claim-value")]
+        public string? IdentityClaimValue { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+
+            var temAcesso = CustomAuthorization.ValidarClaimsUsuario(_contextAccessor.HttpContext, IdentityClaimName, IdentityClaimValue);
+
+            if (temAcesso) return;
+
+            output.Attributes.RemoveAll("href");
+            output.Attributes.Add(new TagHelperAttribute("style", "cursor: not-allowed"));
+            output.Attributes.Add(new TagHelperAttribute("title", "Você não tem permissão"));
+        }
+    }
+}
